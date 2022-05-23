05230e2fd61ef8bddbebaf0cb629d4fff1a05526
jrfnl <jrfnl@users.noreply.github.com>
2022-05-23 02:10:11 +0200

    FeatureComplete/Check: add dedicated test for verbose mode
    These tests verify that:
    * In "verbose" mode, the target directories as resolved will display.

---
+++ Tests/FeatureComplete/Check/VerboseTest.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * PHPCSDevTools, tools for PHP_CodeSniffer sniff developers.
+ *
+ * @package   PHPCSDevTools
+ * @copyright 2019 PHPCSDevTools Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCSStandards/PHPCSDevTools
+ */
+
+namespace PHPCSDevTools\Tests\FeatureComplete\Check;
+
+use PHPCSDevTools\Tests\FeatureComplete\Check\CheckTestCase;
+
+/**
+ * Test "verbose" mode.
+ *
+ * @covers \PHPCSDevTools\Scripts\FeatureComplete\Check::validate
+ */
+final class VerboseTest extends CheckTestCase
+{
+
+    /**
+     * Directory containing the fixtures for the tests in this class.
+     *
+     * @var string
+     */
+    const FIXTURE_DIR = './Tests/Fixtures/FeatureComplete/';
+
+    /**
+     * OS-independent regex for the fixture directory.
+     *
+     * @var string
+     */
+    const FIXTURE_DIR_REGEX = '.+?[\\\\/]Tests[\\\\/]Fixtures[\\\\/]FeatureComplete[\\\\/]';
+
+    /**
+     * Verify verbose mode lists the target directories when a single target directory has been passed.
+     *
+     * @return void
+     */
+    public function testOneTarget()
+    {
+        $command = 'phpcs-check-feature-completeness --no-colors -v ' . self::FIXTURE_DIR . 'ValidStandards/CompleteMixed';
+        $regex   = '`by Juliette Reinders Folmer
+
+Target dir\(s\):
+- ' . self::FIXTURE_DIR_REGEX . 'ValidStandards[\\\\/]CompleteMixed
+
+\.+`';
+
+        $this->runValidation($command, $regex, 0);
+    }
+
+    /**
+     * Verify verbose mode lists the target directories when multiple target directories have been passed.
+     *
+     * @return void
+     */
+    public function testMultipleTargets()
+    {
+        $command = 'phpcs-check-feature-completeness --no-colors -v '
+            . self::FIXTURE_DIR . 'ValidStandards/CompleteMixed '
+            . self::FIXTURE_DIR . 'ValidStandards/CompleteSingleSniff';
+        $regex   = '`by Juliette Reinders Folmer
+
+Target dir\(s\):
+- ' . self::FIXTURE_DIR_REGEX . 'ValidStandards[\\\\/]CompleteMixed
+- ' . self::FIXTURE_DIR_REGEX . 'ValidStandards[\\\\/]CompleteSingleSniff
+
+\.+`';
+
+        $this->runValidation($command, $regex, 0);
+    }
+}
